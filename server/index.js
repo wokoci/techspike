c267d9d8aacf31b08e91bd7cc9fdbc5c757b232b
Jeffrey Wokocha <jeffreyw@kainos.com>
2016-09-23 21:09:51 +0100

    reformat code before merge for use on dashboard

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ var http =require('http');
 let bodyParser =require('body-parser');
 http.get('http://www.google.com/index.html', (res) => {
   console.log(`Got response: ${res.statusCode}`);
-  // consume response body
+
   res.resume();
 }).on('error', (e) => {
   console.log(`Got error: ${e.message}`);
